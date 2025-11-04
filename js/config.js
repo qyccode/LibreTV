408f9cef647d067f4a6c25b77e00c995603addc4
qyccode <admin@qyccc.com>
2025-11-04 17:49:46 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: '影视库',
     url: 'https://v.eeeu.eu',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
